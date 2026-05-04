5926fa7a8b50db83d8d00831a5a0f7a2358dd2ee
Usuario <Usuario@DESKTOP-EOABLUL>
2026-05-04 15:05:54 -0500

    Añadimos el método de insercción del REGISTER - Eccommerce.

---
+++ src/java/Interface/IUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interface;
+
+import Model.Usuario;
+
+/**
+ *
+ * @author James Rios
+ */
+public interface IUsuario {
+    public Usuario validate(String usuario, String clave);
+    
+}
