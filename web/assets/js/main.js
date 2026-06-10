622f43bad2d21f21a76dc8c6ea588db03d393283
Usuario <Usuario@DESKTOP-EOABLUL>
2026-06-10 14:30:20 -0500

    INTEGRACIÓN CON LAS VISTAS - MVC

---
+++ web/assets/js/main.js
@@ -0,0 +1,47 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+// MAIN - JAVAScript FILE
+
+// EXCLUSIVAMENTE PARA HTML
+async function loadComponent(id, file) {
+const response = await fetch(file);
+        const data = await response.text();
+        document.getElementById(id).innerHtml = data;
+        }
+        
+// CARGAR LIBRERIAS - SCRIPT
+function loadScript(src){
+    return new Promise((resolve, reject)=>{
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload=resolve;
+    script.onerror= rejectc
+    document.body.appendChild(script);
+    });
+
+}
+async function init() {
+    try {
+        // CARGAR COMPLEMENTOS HTML
+        await loadComponent('head-placeholder', 'head.html');
+        await loadComponent('header-placeholder', 'header.html');
+        await loadComponent('footer-placeholder', 'footer.html');
+        
+        // CARGAR LIBRERÍAS EN ORDEN ESCTRICTO
+        await loadScript('assets/js/jquery-3.6.0.min.js');
+        await loadScript('assets/js/jquery.dataTables.min.js');
+        await loadScript('assets/js/jquery.dataTables.bootstrap5.min.js');
+        
+        await loadScript('assets/js/jquery.bootstrap.bundle.min.js');
+        await loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11');
+        
+    } catch (e) {
+        
+    }
+
+}
+
+
