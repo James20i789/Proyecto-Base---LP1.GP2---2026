f937258108563d8867997d6e0b8fd41d5348804d
Usuario <Usuario@DESKTOP-EOABLUL>
2026-06-12 11:33:18 -0500

    INTEGRACIÓN CON LAS VISTAS - ASSETS

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -7,19 +7,19 @@
 
 // EXCLUSIVAMENTE PARA HTML
 async function loadComponent(id, file) {
-const response = await fetch(file);
-        const data = await response.text();
-        document.getElementById(id).innerHtml = data;
-        }
-        
+    const response = await fetch(file);
+    const data = await response.text();
+    document.getElementById(id).innerHtml = data;
+}
+
 // CARGAR LIBRERIAS - SCRIPT
-function loadScript(src){
-    return new Promise((resolve, reject)=>{
-    const script = document.createElement('script');
-    script.src = src;
-    script.onload=resolve;
-    script.onerror= rejectc
-    document.body.appendChild(script);
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = rejectc
+        document.body.appendChild(script);
     });
 
 }
@@ -29,19 +29,19 @@ async function init() {
         await loadComponent('head-placeholder', 'head.html');
         await loadComponent('header-placeholder', 'header.html');
         await loadComponent('footer-placeholder', 'footer.html');
-        
+
         // CARGAR LIBRERÍAS EN ORDEN ESCTRICTO
         await loadScript('assets/js/jquery-3.6.0.min.js');
         await loadScript('assets/js/jquery.dataTables.min.js');
-        await loadScript('assets/js/jquery.dataTables.bootstrap5.min.js');
-        
-        await loadScript('assets/js/jquery.bootstrap.bundle.min.js');
-        await loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11');
-        
-    } catch (e) {
-        
-    }
+        await loadScript('assets/js/dataTables.bootstrap5.min.js');
 
+        await loadScript('assets/js/bootstrap.bundle.min.js');
+        await loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11');
+
+    } catch (e) {
+        console.error("Error al cargar la aplicacion", e);
+    }
 }
+init();
 
 
